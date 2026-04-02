b2028cb9cc9214c20a0f0914e8413a4432c795b8
alexmascension <alexmascension@gmail.com>
2026-04-02 14:17:22 +0200

    Updated reload_cloudflare

--- reload_cloudflare.sh
+++ reload_cloudflare.sh
@@ -8,3 +8,10 @@ cellxgene launch /mnt/data/Proyectos/kranocito/data/ARAUZO_03/processed_adatas/c
 # Ctrl + b + c
 cloudflared tunnel --protocol http2 --url http://127.0.0.1:5005 --loglevel info 
 
+
+
+# repeat for oprescu
+cellxgene launch /mnt/data/Proyectos/kranocito/data/processed/oprescu_processed_clean_FAP_with_phate.h5ad --host 127.0.0.1 --port 5006 --disable-annotations --disable-diffexp
+cloudflared tunnel --protocol http2 --url http://127.0.0.1:5006 --loglevel info 
+
+ 
